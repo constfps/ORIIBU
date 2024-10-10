7b619d06d79fc1e40dadf476e8acfae1e20ff9a3
const <constfps@gmail.com>
2024-10-09 21:41:01 -0700

    some soup stuff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "https://osu.ppy.sh/users/33933668"
 r = requests.get(URL)
-print(r.content)
+soup = BeautifulSoup(r.content, 'html5lib')
+print(soup.encode("utf-8"))
